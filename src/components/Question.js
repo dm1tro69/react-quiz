4077357454c390de043dbbcce02d8b509ba848a7
dm1tro69 <dimolg22@gmail.com>
2022-02-06 14:20:06 +0200

    add css

---
+++ src/components/Question.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Answer from "./Answer";
+
+const Question = () => {
+    return (
+        <div>
+            <div className={'question'}>
+               Text of our Question
+            </div>
+            <div className={'answers'}>
+                <Answer/>
+                <Answer/>
+                <Answer/>
+                <Answer/>
+            </div>
+        </div>
+    );
+};
+
+export default Question;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './index.css'
+import Quiz from "./components/Quiz";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Quiz/>
   </React.StrictMode>,
   document.getElementById('root')
 );
